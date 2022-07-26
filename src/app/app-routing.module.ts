6a634f5c8111b78ac29f97ed4e98d20242730a74
Prathviranii <prathvirani2001@gmail.com>
2022-07-26 10:28:30 +0530

    26/7/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: '',component:HomeComponent},
+{path: 'home',component:HomeComponent,canActivate:[AuthGuard]},
+{ path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AuthService]
 })
 export class AppRoutingModule { }
